a6237f04250b6d79b789f375f59a36ed9e72368c
sunshine0904 <sunshineboylei@gmail.com>
2016-06-04 23:28:11 +0800

    modify pkt.h

--- pkt.h
+++ pkt.h
@@ -10,13 +10,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+//layout two protocol
 #include <net/ethernet.h> //ethernet header
 #include <net/if_arp.h> //arp header
+
+//layout three protocol
 #include <linux/ip.h> //ip header
 #include <linux/ipv6.h> // ipv6 header
 #include <linux/icmp.h> //icmp header
 #include <linux/icmpv6.h> //icmpv6 header
 #include <linux/igmp.h> //igmp message
+
+//layout four protocol
+#include <linux/tcp.h> //tcp header
+#include <linux/udp.h> //udp header
+
+//layout application protocol
+//such as telnet、http、 dns、ftp、ssl and so on.
+
 
 
 #if 0
