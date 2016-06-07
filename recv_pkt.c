4151d6183f87808bb511d31f6604375a71f25cf6
sunshine0904 <sunshineboylei@gmail.com>
2016-06-07 21:53:12 +0800

    modify recv function

--- recv_pkt.c
+++ recv_pkt.c
@@ -7,32 +7,34 @@
 #endif
 
 
-int creat_rawsocket();
-int bind_socket_dev(u32_t socket,u8_t *device);
+u32_t creat_rawsocket();
+u32_t bind_socket_dev(u32_t socket,u8_t *device);
+u32_t analysis_data(u8_t *data);
 
 
-
-int main()
+u32_t main(int argc,char **argv)
 {
+	u8_t *dev = NULL;
+	if(argc == 1)
+	{
+		dev = "eth0";
+	}
+	else if(argc > 1)
+	{
+		dev = argv[1];
+	}
+	
 	u32_t raw_sock = 0;
 	struct sockaddr_in recvaddr;
 	u32_t len = sizeof(struct sockaddr);
 	u8_t *buff = (u8_t *)malloc(BUFF_SIZE);
-	u32_t i = 0,ret = 0;	
-	u8_t *dev = "eth0";
-
-
-	struct ether_header *ethhdr;
-
-
+	u32_t ret = 0;	
 
 	//creat raw socket
 	raw_sock = creat_rawsocket();
 
 	//bind device to eth0
 	bind_socket_dev(raw_sock,dev);
-
-
 
 	while(1)
 	{
@@ -42,28 +44,12 @@ int main()
 			continue;
 		}
 
-		//display ether header
-		ethhdr = buff;
-		printf("\n++++ether header++++\n");
-		printf("eth_src_mac:");
-		for(i = 0;i<6;i++)
-		{
-			printf("%02x ",ethhdr->ether_shost[i]);
-		}
-		printf("\n");
-		printf("eth_dst_mac:");
-		for(i = 0;i<6;i++)
-		{
-			printf("%02x ",ethhdr->ether_dhost[i]);
-		}
-		printf("\n");
-		printf("eth_type:%#06x\n",htons(ethhdr->ether_type));
-
+		analysis_data(buff);
 
 	}
 }
 
-int creat_rawsocket()
+u32_t creat_rawsocket()
 {
 	u32_t raw_sock = socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL));
 	if(raw_sock == 0)
@@ -74,7 +60,7 @@ int creat_rawsocket()
 	printf("creat raw socket ok\n");
 	return raw_sock;
 }
-int bind_socket_dev(u32_t socket,u8_t *device)
+u32_t bind_socket_dev(u32_t socket,u8_t *device)
 {
 	u32_t index = if_nametoindex(device);
 	printf("device:%s index:%d\n",device,index);
@@ -88,5 +74,45 @@ int bind_socket_dev(u32_t socket,u8_t *device)
 	{
 		printf("bind device error\n");
 		return -1;
+	}
+}
+u32_t analysis_data(u8_t *data)
+{
+	//display ether header
+	u32_t i = 0;
+	struct ether_header *ethhdr = data;;
+	printf("\n++++ether header++++\n");
+	printf("eth_src_mac:");
+	for(i = 0;i<6;i++)
+	{
+		printf("%02x ",ethhdr->ether_shost[i]);
+	}
+	//printf("\n");
+	printf("    ");
+	printf("eth_dst_mac:");
+	for(i = 0;i<6;i++)
+	{
+		printf("%02x ",ethhdr->ether_dhost[i]);
+	}
+	printf("\n");
+	printf("eth_type:%#06x\n",htons(ethhdr->ether_type));
+	
+	switch(htons(ethhdr->ether_type))
+	{
+		case ETHERTYPE_IP:
+			printf("it is ipv4 packet\n");
+			break;
+		case ETHERTYPE_IPV6:
+			printf("it is ipv6 packet\n");
+			break;
+		case ETHERTYPE_ARP:
+			printf("it is arp packet\n");
+			break;
+		case ETHERTYPE_REVARP:
+			printf("it is revarp packet\n");
+			break;
+		default:
+			printf("can't parse type! eth_type:%02x\n",htons(ethhdr->ether_type));
+			break;
 	}
 }
